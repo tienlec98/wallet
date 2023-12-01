c11a02e4087440ff7df987c6e709bb6db0a5a577
tienle <leviettien456789@gmail.com>
2023-12-01 15:38:28 +0700

    remove bip39

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import { STANDARD, Wallet } from './types'
 import { privateKeyToAddress, convertPrivateKey, publicKeyToAddress, generateMasterKey } from './services'
 import { generateParam } from './common'
-const bip39 = require('@coin98/bip39')
-
 
 export default class Cryptography {
   singleChain = async (chain: string, sMnemonic: string, standard: STANDARD, isPrivateKey: boolean = false ): Promise<Wallet> => {
@@ -15,9 +13,6 @@ export default class Cryptography {
     }
 
     let mnemonic = sMnemonic
-    if (!sMnemonic) {
-      mnemonic = await this.genSeedPhrase()
-    }
 
     const {algo, path} = generateParam(chain, standard)
 
@@ -38,9 +33,5 @@ export default class Cryptography {
       privateKeyBuffer: masterSeed.privateKey
     }
     return wallet
-  }
-  genSeedPhrase = async (bytes: number = 128): Promise<string> => {
-    const mnemonic = await bip39.generateMnemonic(bytes)
-    return mnemonic
   }
 }
